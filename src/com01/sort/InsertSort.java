d211337dcf96f0028c8e72790d62fab96cccb0d9
MrTaozui <965369592@qq.com>
2019-09-21 17:40:27 +0800

    添加说明

--- src/com01/sort/InsertSort.java
+++ src/com01/sort/InsertSort.java
@@ -22,10 +22,10 @@ public class InsertSort {
 
         for (int i = 1; i < arr.length; i++) { //i 是待插入的当前索引
             int insertIndex = i;//要插入的地方
-            int isnertNum = arr[i];//待插入的数
+            int isnertNum = arr[i];//待插入的数   因为数组是移动的 所以 要保存此值
 
             for (int j = i - 1; j >= 0; j--) {//依次与前面的数比较
-                if (isnertNum < arr[j]) {
+                if (isnertNum < arr[j]) { //用到保存值得地方
                     arr[j + 1] = arr[j];//往后移
                     insertIndex = j;
                 } else {
