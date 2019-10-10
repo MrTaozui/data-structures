787d12e930ec380e2ee25a81b209221f920fa4ba
MrTaozui <965369592@qq.com>
2019-10-10 17:49:54 +0800

    完全二叉树的顺序存储遍历

--- src/com01/tree/threadedTree/ThreadedTreeDemo.java
+++ src/com01/tree/threadedTree/ThreadedTreeDemo.java
@@ -25,7 +25,10 @@ public class ThreadedTreeDemo {
         tree.midThreadedNodes();
         System.out.println(node4.getLeft());
         System.out.println(node4.getRigtht());
+        System.out.println(node4.getFlagR());
         System.out.println(node5.getLeft());
+        System.out.println("--------线索化遍历---------");
+        tree.midThreadedNodesList();
 
     }
 
@@ -98,6 +101,34 @@ class ThreadedBinaryTree {
 
     }
 
+
+    //中序线索化二叉树的遍历  这样遍历的方式，符合中序遍历的时候 程序遍历节点时候代码运行的方式
+    public void midThreadedNodesList(){
+
+        HeroNode curr = root;
+
+        while (curr!=null){
+            while (curr.getFlagL()==0){//找到最左边的节点
+                curr = curr.getLeft();
+            }
+            System.out.println(curr);//打印当前节点
+            while (curr.getFlagR()==1){//有后继节点就一直输出
+                curr = curr.getRigtht();//到达后继节点
+                System.out.println(curr);
+            }
+            curr = curr.getRigtht();//说明这个节点有右子树 把当前节点作为根节点 继续遍历
+
+            //说明理解  拿最简单的三元素的二叉树来说（2为根1为左3为右），先找到最左边的节点1，输出，然后1的后继2 输出，
+            //这时 2 左右都有孩子 但是2已经作为1的后继遍历过了，所以找到右节点 输出（单个节点3也是1的子树）他既是最左边
+            //也是最后边 所以会输出
+
+            //然后（2，1，3）遍历完成后 它可以作为其他节点的左子树， 然后递归遍历
+
+
+        }
+
+
+    }
 
     public HeroNode getRoot() {
         return root;
