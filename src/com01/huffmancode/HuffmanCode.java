78841f7f02c5f28562b40f70bf88deb46cf335fe
MrTaozui <965369592@qq.com>
2019-10-15 11:18:47 +0800

    哈夫曼解压字符串

--- src/com01/huffmancode/HuffmanCode.java
+++ src/com01/huffmancode/HuffmanCode.java
@@ -13,10 +13,18 @@ public class HuffmanCode {
 
     public static void main(String[] args) {
         String str = "i like like like java do you like a java";
+        //String str = "ab"; //会有问题 因为最后一步的处理01  会变成1
         //Map<Byte, String> huffmanTable = genHuffmanCode(str);
         //huffmanTable.forEach((k,v)-> System.out.println(k+":"+v));
         byte[] zip = zip(str);
+        // 10101000(补码) => byte  [推导  10101000=> 10101000 - 1 => 10100111(反码)=> 11011000= -88 ]
+        //
         System.out.println(Arrays.toString(zip));
+
+
+        System.out.println(unzip(zip,huffmanCodeMap));
+//        System.out.println(Integer.parseInt("00101",2));
+//        System.out.println(Integer.toBinaryString(5));
 
 
     }
@@ -44,7 +52,7 @@ public class HuffmanCode {
         for (byte key:nodeKeySet){
             list.add(nodeMap.get(key));
         }
-        list.forEach( x->System.out.println(x));
+        //list.forEach( x->System.out.println(x));
         while (list.size()>1) {
             Collections.sort(list);
             Node left = list.get(0);//左子树
@@ -101,7 +109,8 @@ public class HuffmanCode {
 
     }
 
-    //压缩
+    // huffmanCodeBytes[0] =  10101000(补码) => byte  [推导  10101000=> 10101000 - 1 => 10100111(反码)=> 11011000= -88 ]
+    //二进制转为字符串是补码的形式展示的，所以 原码推导为上步骤 补码结果是-40 ，但是-88按照字符串打印还是补码，即二进制展示是补码展示
     public static byte[] zip(String str){
         Map<Byte, String> huffmanTable = genHuffmanCode(str);
         byte[] bytes = str.getBytes();
@@ -110,6 +119,7 @@ public class HuffmanCode {
         for (byte byt:bytes){
             strCode.append(huffmanTable.get(byt));
         }
+//"1010100010111111110010001011111111001000101111111100100101001101110001110000011011101000111100101000101111111100110001001010011011100"
        // System.out.println(strCode.toString());
         int length = (strCode.length()+7)/8;//长度不足8的倍数的时候 补足
         byte[] result = new byte[length];
@@ -128,6 +138,77 @@ public class HuffmanCode {
 
     }
 
+    /**
+     *
+     * @param byt 需要转码的字符
+     * @param flag 是否需要补高位 因为6是0000 0110转过来的 所以需要补足回去 true 需要 false 不需要
+     */
+    public static String byteToBitString(byte byt,boolean flag){
+        String str=null;
+        int tmp = byt;
+        if(flag){
+            str=  Integer.toBinaryString(tmp|=256);
+            str = str.substring(str.length()-8);//截取后8位
+
+        }else {
+            str = Integer.toBinaryString(tmp);
+        }
+        //System.out.println(str);
+       return str;
+
+    }
+
+
+
+    /**
+     * 解压
+     * @param bytes 待解压字节数组
+     * @param huffmanTable 哈夫曼编码表
+     * @return
+     */
+    public static String unzip(byte[] bytes,Map<Byte,String> huffmanTable){
+        StringBuilder decode = new StringBuilder();
+        List<Byte> result =new ArrayList<>();//中间转码存储
+        Map<String,Byte> table = new HashMap<>();
+
+
+        for (Map.Entry<Byte,String> entry:huffmanTable.entrySet()){//反转哈夫曼编码表
+            table.put(entry.getValue(),entry.getKey());
+        }
+
+        //把byte 翻译成哈夫曼后的字节码
+        for (int i=0;i<bytes.length-1;i++){
+           String decodeStr =  byteToBitString(bytes[i],true);
+            decode.append(decodeStr);
+        }
+        String decodeStr = byteToBitString(bytes[bytes.length-1],false);//最后一位不需要调 因为以前压缩的时候 8位一个 剩余的变成了一个
+        decode.append(decodeStr);
+
+
+        for (int i = 0; i <decode.length(); ) {//转换成原字符
+            int k=1;
+            while (true){
+                Byte strByt = table.get(decode.substring(i,i+k));
+                if(strByt==null){
+                    k++;
+                }else {
+                    result.add(strByt);
+                    i+=k;
+                    break;
+                }
+            }
+
+        }
+
+        byte[] resultByts = new byte[result.size()];//构造返回结果
+        for (int i = 0; i <result.size() ; i++) {
+            resultByts[i] = result.get(i);
+
+        }
+        return new String(resultByts);
+
+
+    }
 
 
 }
